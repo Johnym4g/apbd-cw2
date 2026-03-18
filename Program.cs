1ccca644256d3d3270553661ecfd480ab04f4411
Jan Głogowski <jan.glogowski@onet.pl>
2026-03-18 15:54:20 +0100

    implement: basic input handling

--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter numbers separated by spaces (e.g. 1 5 10):");
+string? input = Console.ReadLine();
+
+Console.WriteLine("User entered: " + input);
