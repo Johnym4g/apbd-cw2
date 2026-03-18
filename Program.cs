fb6b10ba49b832caa2e38da08088eda217246bc7
Jan Głogowski <jan.glogowski@onet.pl>
2026-03-18 16:13:08 +0100

    add: CalculateAverage method

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 ﻿Console.WriteLine("Enter numbers separated by spaces (e.g. 1 5 10):");
 string? input = Console.ReadLine();
 
-Console.WriteLine("User entered: " + input);
-
 if (string.IsNullOrWhiteSpace(input))
 {
     Console.WriteLine("Error: Input cannot be empty! Please provide some numbers.");
     return;
 }
+
+double CalculateAvarage(int[] numbers)
+{
+    return numbers.Average();
+}
+Console.WriteLine($"Avarage: {CalculateAvarage(input.Split(' ').Select(int.Parse).ToArray())}");
